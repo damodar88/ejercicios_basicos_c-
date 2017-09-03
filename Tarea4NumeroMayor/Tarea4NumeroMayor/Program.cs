b8f9ee506e7712e161e181b3ffb3c8ae8021265f
damodar <damodar@openmailbox.org>
2017-09-03 18:07:59 +0200

    Tarea 3 if en C#

--- Tarea4NumeroMayor/Tarea4NumeroMayor/Program.cs
+++ Tarea4NumeroMayor/Tarea4NumeroMayor/Program.cs
@@ -10,10 +10,21 @@ namespace Tarea4NumeroMayor
     {
         static void Main(string[] args)
         {
+            int num1,num2;
 
+            Console.WriteLine("Ingrese el primer numero--:");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingrese el segundo numero--:");
+            num2 = Convert.ToInt32(Console.ReadLine());
 
-
-
+            if(num1>num2)
+            {
+            
+            Console.WriteLine("El primer numero {0} es mayor",num1);
+            }else{
+            
+            Console.WriteLine("El segundo numero {0} ingresado es mayor",num2);
+            }
 
 
 
