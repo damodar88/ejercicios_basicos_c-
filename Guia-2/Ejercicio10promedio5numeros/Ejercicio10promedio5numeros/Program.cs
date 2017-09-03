6231300cd38b8cef2af8644459d351df1b4f4bcc
damodar <damodar@openmailbox.org>
2017-09-03 22:26:25 +0200

    Ejercicio 9 listo

---
+++ Guia-2/Ejercicio10promedio5numeros/Ejercicio10promedio5numeros/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio10promedio5numeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("----------Promedio de 5 numeros-------------");
+
+
+        }
+    }
+}
---
+++ Guia-2/Ejercicio9/Ejercicio9/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Numero + o -");
+
+            Identificador identi = new Identificador();
+
+            identi.calculaNumero();
+
+
+         
+
+        }
+    }
+}
